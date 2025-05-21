d38735aab73322d86944f191a9b2f703c1e9cf53
Felix Cederblad <felix_cederblad@hotmail.com>
2025-05-21 15:58:46 +0200

    Version 1.0

---
+++ PersonnelRegister/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffRegister
+{
+    internal class Employee
+    {
+        private string name;
+        private int salary;
+
+        public Employee(string name, int salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+        public int getSalary()
+        { 
+            return salary; 
+        }
+        public void setName(string name) 
+        { 
+            this.name = name; 
+        }
+        public void setSalary(int salary) 
+        { 
+            this.salary = salary; 
+        }
+
+        public string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(name);
+            sb.Append(' ');
+            sb.Append(salary);
+            return sb.ToString();
+        }
+
+
+    }
+}
--- PersonnelRegister/Program.cs
+++ PersonnelRegister/Program.cs
@@ -1,10 +1,138 @@
-﻿namespace StaffRegister
+﻿using System.Text.RegularExpressions;
+
+namespace StaffRegister
 {
     internal class Program
     {
+        private static List<Employee> employeeList = new List<Employee>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            initialize();
+            run();
+            exit();
+        }
+
+        private static void initialize()
+        {
+            Console.WriteLine("Välkommen till personalregistret!");
+            Console.WriteLine("");
+
+            int numberOfStaff = 0;
+            bool correctInput = false;
+
+            while (!correctInput)
+            {
+                Console.WriteLine("Mata in hur många anställda du har som en siffra: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out numberOfStaff) && numberOfStaff > 0)
+                {
+                    correctInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Du skrev in i fel format, testa igen");
+                    Console.WriteLine();
+                }
+            }
+
+
+
+            for (int i = 0; i < numberOfStaff; i++)
+            {
+                Console.WriteLine("Detta är anställd nr " + (i + 1));
+
+                string name = "";
+                bool correctNameInput = false;
+
+                while (!correctNameInput)
+                {
+                    Console.WriteLine("Mata in dennes namn (endast bokstäver): ");
+                    name = Console.ReadLine();
+
+                    if (Regex.IsMatch(name, @"^[a-zA-ZåäöÅÄÖ\s]+$"))
+                    {
+                        correctNameInput = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du skrev in i fel format, testa igen");
+                        Console.WriteLine();
+                    }
+                }
+
+                int salary = 0;
+                bool correctSalaryInput = false;
+
+                while (!correctSalaryInput)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Mata in dennes lön som en siffra: ");
+                    if (int.TryParse(Console.ReadLine(), out salary) && salary > 0)
+                    {
+                        correctSalaryInput = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du skrev in i fel format, testa igen");
+                        Console.WriteLine();
+                    }
+                }
+
+                Employee anställd = new Employee(name, salary);
+                employeeList.Add(anställd);
+            }
+
+        }
+        private static void run()
+        {
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Vad vill du göra? Skriv in motsvarande siffra: ");
+                Console.WriteLine("1. Se anställda och deras lön?");
+                Console.WriteLine("2. Avsluta programmet");
+
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out int answer))
+                {
+                    Console.WriteLine("Du skrev in i fel format, testa igen");
+                    Console.WriteLine();
+                    continue;
+                }
+
+                if (answer == 1)
+                {
+                    Console.WriteLine();
+                    if (employeeList.Count == 0)
+                    {
+                        Console.WriteLine("Det finns inga anställda att visa.");
+                    }
+                    else
+                    {
+                        foreach (var employee in employeeList)
+                        {
+                            Console.WriteLine(employee.ToString());
+                        }
+                    }
+                }
+                else if (answer == 2)
+                {
+                    running = false;
+
+                }
+                else
+                {
+                    Console.WriteLine("Du skrev in i fel format, testa igen");
+                }
+            }
+        }
+
+        private static void exit()
+        {
+            Console.WriteLine("Nu stängs personalregistret");
         }
     }
 }
